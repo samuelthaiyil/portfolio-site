81b96169ed8dd49890b48666ee5fa201d7b3a7fa
samuelbluefin <samuel@bluefinenterprises.com>
2024-07-07 05:36:42 -0400

    implemented link tag for contact page cta

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,11 @@ export const Hero = () => (
             a versatile software developer with a focus on creating tangible impact.
         </h1>
         <div className="py-5 flex justify-center">
-            <a href="/contact" className="text-2xl bg-blue-600 text-white py-4 px-5 rounded hover:bg-blue-500 transition hover:duration-300 group">
-                Let's connect <span className="inline-block transition-transform transform group-hover:translate-x-1">-&gt;</span>
-            </a>
+            <Link href="/contact">
+                <a className="text-2xl bg-blue-600 text-white py-4 px-5 rounded hover:bg-blue-500 transition hover:duration-300 group">
+                    Let's connect <span className="inline-block transition-transform transform group-hover:translate-x-1">-&gt;</span>
+                </a>
+            </Link>
         </div>
     </>
 )
