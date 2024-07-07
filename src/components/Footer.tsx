d68c9aabc2f9fa51ce2214f8663dc7614c328eaa
samuelbluefin <samuel@bluefinenterprises.com>
2024-07-07 18:36:05 -0400

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="absolute bottom-0 w-full py-4 border">
+            <p className="text-center">by Sam</p>
+        </div>
+    )
+}
