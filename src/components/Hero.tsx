2d27366ab1259008e311639eecb378b637d65413
samuelbluefin <samuel@bluefinenterprises.com>
2024-07-07 05:30:43 -0400

    added preliminary homepage

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+export const Hero = () => (
+    <>
+        <h1 className="font-bold text-5xl text-center p-32 pb-8">
+            Hi! I'm Sam,
+            a versatile software developer with a focus on creating tangible impact.
+        </h1>
+        <div className="py-5 flex justify-center">
+            <a href="/contact" className="text-2xl bg-blue-600 text-white py-4 px-5 rounded hover:bg-blue-500 transition hover:duration-300 group">
+                Let's connect <span className="inline-block transition-transform transform group-hover:translate-x-1">-&gt;</span>
+            </a>
+        </div>
+    </>
+)
