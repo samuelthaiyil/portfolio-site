93b719be33d3fa1f83a8a40cf1d98b607d56ac81
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-08 17:17:45 -0400

    added about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import {Hero} from "@/components/Hero";
 import Footer from "@/components/Footer";
+import {About} from "@/components/About";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <About />
             <Footer />
         </>
     )
