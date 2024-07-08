3e796c26821beabc20d702b29e3f825f66072f1f
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-08 17:17:55 -0400

    tweaked footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="absolute bottom-0 w-full py-4 border">
-            <p className="text-center">by Sam</p>
+            <p className="text-center text-slate-400">2024 &#169; by Sam</p>
         </div>
     )
 }
