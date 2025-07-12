0106fc6a3172cb51d7788e7e9cfe69e8d1a8ec41
sampledgx <samuel@pledgx.com>
2025-07-12 13:24:26 -0400

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="absolute bottom-0 w-full py-4 border-t border-gray-200 dark:border-gray-700">
+        <div className="absolute bottom-0 w-full py-4">
             <p className="text-center text-slate-400 dark:text-slate-500">{new Date().getFullYear()} &#169; by Sam</p>
         </div>
     )
