0014374708719b1f80068f7dc744855301768fab
sampledgx <samuel@pledgx.com>
2025-07-12 12:47:26 -0400

    added description

---
+++ src/components/Description.tsx
@@ -0,0 +1,5 @@
+export const Description = () => (
+    <div className="text-gray-700 dark:text-gray-300 px-8 py-8 text-center max-w-4xl mx-auto text-xl">
+I build intelligent systems that solve complex problems. As a Full Stack Engineer at a Toronto fintech startup, I architect and deploy AI workflows that drive operational efficiency in financial systems. I earned my BSc in Computer Science from Lakehead University and am completing my MSc in Computer Information Systems at Boston University to stay at the forefront of emerging technologies.
+    </div>
+)
