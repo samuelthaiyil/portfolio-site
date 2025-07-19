05594e68fe2483398bd4a4d46e3fc0ae72af09f7
sampledgx <samuel@pledgx.com>
2025-07-19 14:43:48 -0400

    updated posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Instrument_Sans } from "next/font/google";
+import { useEffect } from 'react';
+import posthog from 'posthog-js';
 
 const instrumentSans = Instrument_Sans({ 
   subsets: ["latin"],
@@ -11,6 +13,15 @@ const instrumentSans = Instrument_Sans({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+            posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+                api_host: "/ingest",
+                ui_host: "https://us.posthog.com",
+                debug: process.env.NODE_ENV === "development",
+            });
+        }
+    }, []);
     return (
         <div className={instrumentSans.className}>
             <ThemeProvider>
