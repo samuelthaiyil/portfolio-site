9d2e4115d205406d97115d33112607476bdd9b1a
sampledgx <samuel@pledgx.com>
2025-07-20 12:39:21 -0400

    escaped apostrophes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -53,7 +53,7 @@ export default function About() {
                                 </h1>
                                 <div className="space-y-6">
                                     <p>
-                                        I'm a Full Stack Engineer at PledgX, a Toronto based fintech startup. I use React, Typescript and Python Flask to architect and deploy AI workflows that drive operational efficiency in financial systems.
+                                        I&apos;m a Full Stack Engineer at PledgX, a fintech startup. I use React, Typescript and Python Flask to architect and deploy AI workflows that drive operational efficiency in financial systems.
                                     </p>
                                     <p>
                                         I first developed a passion for building at age 12, teaching myself C++ and Direct3D to create a 3D game engine. This early spark led me to earn my BSc in Computer Science from Lakehead University, and I&apos;m currently completing my MSc in Computer Information Systems with a concentration in Data Analytics at Boston University to stay at the forefront of emerging technologies.
