e9d98064af563e9968b777d6ef15da7402b2e87e
sampledgx <samuel@pledgx.com>
2025-08-17 18:28:01 -0400

    added projects section

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,13 +13,13 @@ export const Nav = () => {
                             ? "dark:text-white tracking-wider" 
                             : "text-slate-400 hover:text-slate-500 dark:text-slate-300 dark:hover:text-slate-200"
                     }`} href="/">Home</Link>
-                    {/* <Link className={router.pathname === "/experience" ? "text-xl font-bold dark:text-white" : "text-xl font-bold text-slate-400 hover:text-slate-500 dark:text-slate-300 dark:hover:text-slate-200 hover:duration-200"} href="/experience">Experience</Link>
-                    <Link className={router.pathname === "/projects" ? "text-xl font-bold dark:text-white" : "text-xl font-bold  text-slate-400 hover:text-slate-500 dark:text-slate-300 dark:hover:text-slate-200 hover:duration-200"} href="/projects">Projects</Link> */}
                     <Link className={`text-xl font-bold transition-all duration-500 ease-out hover:tracking-wider ${
                         router.pathname === "/about" 
                             ? "dark:text-white tracking-wider" 
                             : "text-slate-400 hover:text-slate-500 dark:text-slate-300 dark:hover:text-slate-200"
                     }`} href="/about">About</Link>
+                     <Link className={router.pathname === "/projects" ? "text-xl font-bold dark:text-white" : "text-xl font-bold  text-slate-400 hover:text-slate-500 dark:text-slate-300 dark:hover:text-slate-200 hover:duration-200"} href="/projects">Projects</Link> 
+                   
                     {/* <Link className={`text-xl font-bold transition-all duration-500 ease-out hover:tracking-wider ${
                         router.pathname === "/contact" 
                             ? "dark:text-white tracking-wider" 
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,25 +4,11 @@ type Project = { name: string; stack: string; description: string; url: string; 
 
 const projects: Project[] = [
     {
-        name: "Cryptocurrency Price Viewer",
-        stack: "JavaFX, Desktop",
-        description: "Developed a cross-platform desktop application using Java and JavaFX to view cryptocurrency prices. Utilized Coinbase REST APIs to fetch BTC and ETH price data, and displayed historical prices in a line graph.",
-        url: "https://github.com/samuelthaiyil/CryptoPriceWatcher"
+        name: "Searchr AI",
+        stack: "React, Next.js, TypeScript, Convex, TwelveLabs, Mux",
+        description: "A video searching platform that leverages vision AI and transcription to find relevant clips based on the users' search based on a library of videos",
+        url: "https://www.loom.com/share/d304e8eee6a24b4690b33736873cd3d5?sid=0302b3b2-71c0-4fef-92c1-8956b17b1c3e"
     },
-    {
-        name: "Starter",
-        stack: "Swift, iOS",
-        description: "Created a solution for athletes in gaining easier exposure by building a social media style iOS application with Swift and\n" +
-            "GCP. Users could upload video clips to a profile and edit the clips through an in app video editor.",
-        url: "https://github.com/samuelthaiyil/StarterApp"
-    },
-    {
-        name: "Statify",
-        stack: ".NET C#, Web",
-        description: "Reduced the need for manual stat taking at sports events by creating an interactive web application using ASP.NET MVC,\n" +
-            "MSSQL Server and Azure to allow users to create teams and add players and games attached to players to track statistics.",
-        url: "https://github.com/samuelthaiyil/COMP2084G_BasketballStatTracker"
-    }
 ]
 
 export default function Projects() {
@@ -30,7 +16,6 @@ export default function Projects() {
         <>
             <Nav/>
             <div className="px-8">
-                <h1 className="text-3xl lg:text-4xl font-bold text-center py-8 lg:py-12">Projects</h1>
                 {
                     projects.map(({name, stack, description, url}) => {
                         return (
@@ -41,7 +26,7 @@ export default function Projects() {
                                 <div className="py-5">
                                     <a href={url} target="_blank"
                                        className="bg-blue-600 text-white py-2 px-5 mt-8 rounded hover:bg-blue-500 transition hover:duration-300 group">
-                                        View Project <span
+                                        Watch Demo <span
                                         className="inline-block transition-transform transform group-hover:translate-x-1">-&gt;</span>
                                     </a>
                                 </div>
